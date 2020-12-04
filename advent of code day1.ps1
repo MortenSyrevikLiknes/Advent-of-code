ece8eab9ebca26f37665b3ed53030946c9d15b38
Morten Liknes <mliknes@hotmail.com>
2020-12-04 18:58:53 +0100

    Add solution to task1

---
+++ advent of code day1.ps1
@@ -0,0 +1,15 @@
+﻿#Advent of code day1, task1
+$rawInput= Get-Content -Path C:\users\mlikn\Downloads\input
+$listOfNumb= foreach($n in $rawInput){$n -as [int]}
+foreach($n in $listOfNumb)
+    {
+        foreach($n2 in $listOfNumb)
+            {
+               if ($n+$n2 -eq 2020)
+               {
+                    $sum=$n*$n2
+                    Write-Output $sum
+
+               }
+            }
+    }
